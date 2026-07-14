3e7b413f9d815b149e9921da5a397a31fe9f2475
aarthquake <ak3528@scarletmail.rutgers.edu>
2026-07-13 22:11:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,23 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-drive_base = DriveBase(left_motor, right_motor, wheel_diameter=55, axle_track=118)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55, axle_track=118)
+opticalSensor = UltrasonicSensor(Port.S4)
 
+drive_speed = 100
+robot.straight(1000)
 
 # Write your program here.
 ev3.speaker.beep()
+
+while True:
+    distance = opticalSensor.distance()
+    if distance < 200:
+        robot.stop()
+        ev3.speaker.say("Obstacle detected")
+        wait(1000)
+        robot.straight(-100)
+        robot.turn(90)
+        robot.straight(100)
+    else:
+        robot.drive(drive_speed, 0)
