c8760d0582f9bf0368ce73c7fa68e2985237f140
modus09 <mabbybright@gmail.com>
2026-07-15 20:22:20 -0400

    added line following + audio telemetry

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env pybricks-micropython
+
+from ast import While
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.nxtdevices import (LightSensor, SoundSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -17,22 +21,35 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55, axle_track=118)
-opticalSensor = UltrasonicSensor(Port.S4)
+lightSensor = LightSensor(Port.S3)
 
-drive_speed = 100
-robot.straight(1000)
+#opticalSensor = UltrasonicSensor(Port.S4)
+
+drive_speed = 500
+#robot.straight(1000)
 
 # Write your program here.
-ev3.speaker.beep()
 
 while True:
-    distance = opticalSensor.distance()
-    if distance < 200:
-        robot.stop()
-        ev3.speaker.say("Obstacle detected")
-        wait(1000)
-        robot.straight(-100)
-        robot.turn(90)
-        robot.straight(100)
+    absorbance = lightSensor.reflection()
+    ev3.speaker.bep()
+    ev3.speaker.say("Absorbance is " + str(absorbance))
+    wait(2000)
+#line following loop
+    if absorbance < 50:
+        robot.drive(drive_speed, 30)
     else:
-        robot.drive(drive_speed, 0)
+        robot.drive(drive_speed, -30)
+
+#obstacle avoidance loop
+#  while True:
+#     distance = opticalSensor.distance()
+#     if distance < 200:
+#         robot.stop()
+#         ev3.speaker.say("Obstacle detected")
+#         wait(1000)
+#         robot.straight(-100)
+#         robot.turn(90)
+#         robot.straight(100)
+#     else:
+#         robot.drive(drive_speed, 0)
